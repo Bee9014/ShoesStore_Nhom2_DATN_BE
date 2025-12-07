bd82f0fec916722194bb2a333574e9f9473089be
quanle534 <letran05031107@gmail.com>
2025-12-07 14:29:17 +0700

    đẩy lại code payment

---
+++ src/main/java/com/fpl/edu/shoeStore/payment/convert/PaymentConverter.java
@@ -0,0 +1,48 @@
+package com.fpl.edu.shoeStore.payment.convert;
+
+import com.fpl.edu.shoeStore.payment.dto.request.PaymentDTORequest;
+import com.fpl.edu.shoeStore.payment.dto.response.PaymentDTOResponse;
+import com.fpl.edu.shoeStore.payment.entity.Payment;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PaymentConverter {
+    public static Payment toEntity(PaymentDTORequest dto) {
+        if (dto == null) return null;
+
+        Payment payment = new Payment();
+
+        payment.setOrderId(dto.getOrderId());
+        payment.setPaymentMethod(dto.getPaymentMethod());
+        payment.setAmount(dto.getAmount());
+        payment.setTransactionCode(dto.getTransactionCode());
+
+        return payment;
+    }
+
+    public static PaymentDTOResponse toDTO(Payment payment) {
+        if (payment == null)     return null;
+
+        return PaymentDTOResponse.builder()
+                .orderId(payment.getOrderId())
+                .paymentId(payment.getPaymentId())
+                .payerId(payment.getPayerId())
+                .paymentMethod(payment.getPaymentMethod())
+                .paymentDate(payment.getPaymentDate())
+                .amount(payment.getAmount())
+                .status(payment.getStatus())
+                .transactionCode(payment.getTransactionCode())
+                .createdAt(payment.getCreatedAt())
+
+                .build();
+    }
+
+    public static List<PaymentDTOResponse> toDTOList(List<Payment> payments) {
+        if (payments == null || payments.isEmpty()) return List.of();
+
+        return payments.stream()
+                .map(PaymentConverter::toDTO)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/com/fpl/edu/shoeStore/payment/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,117 @@
+package com.fpl.edu.shoeStore.payment.service.impl;
+
+import com.fpl.edu.shoeStore.common.handler.PageResponse;
+import com.fpl.edu.shoeStore.payment.convert.PaymentConverter;
+import com.fpl.edu.shoeStore.payment.dto.request.PaymentDTORequest;
+import com.fpl.edu.shoeStore.payment.dto.response.PaymentDTOResponse;
+import com.fpl.edu.shoeStore.payment.entity.Payment;
+import com.fpl.edu.shoeStore.payment.mapper.PaymentMapper;
+import com.fpl.edu.shoeStore.payment.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentServiceImpl implements PaymentService {
+
+    private final PaymentMapper paymentMapper;
+//    private final OrderMapper orderMapper;
+
+    @Override
+    public PageResponse<PaymentDTOResponse> findAllPaged(Integer paymentId, Integer orderId, Integer payerId, String paymentMethod, LocalDateTime paymentDate, String status, BigDecimal amount, String transactionCode, int page, int size) {
+        int offset = (page - 1) * size;
+
+        List<Payment> payments = paymentMapper.findAllPaged
+                (paymentId, orderId, payerId, paymentMethod, paymentDate, status, amount, transactionCode, offset, size);
+
+        long totalElements = paymentMapper.countAll
+                (paymentId, orderId, payerId, paymentMethod, paymentDate, status, amount, transactionCode);
+
+        List<PaymentDTOResponse> paymentDTO = PaymentConverter.toDTOList(payments);
+
+        int totalPages = (int) Math.ceil((double) totalElements / size);
+
+        return PageResponse.<PaymentDTOResponse>builder()
+                .content(paymentDTO)
+                .pageNumber(page)
+                .pageSize(size)
+                .totalElements(totalElements)
+                .totalPages(totalPages)
+                .build();
+    }
+
+    @Override
+    @Transactional
+    public PaymentDTOResponse createPayment(PaymentDTORequest request) {
+//        Order order = orderMapper.findById((long)request.getOrderId());
+//        if (order == null) {
+//            throw new RuntimeException("Đơn hàng không tồn tại!");
+//        }
+//        if (request.getAmount().compareTo(order.getTotalPrice()) != 0){
+//            throw new RuntimeException("Sai lệch số tiền thanh toán !");
+//        }
+
+        Payment payment = PaymentConverter.toEntity(request);
+
+        payment.setStatus("pending");
+        payment.setCreatedAt(LocalDateTime.now());
+        if (payment.getPaymentDate() == null) {
+            payment.setPaymentDate(LocalDateTime.now());
+        }
+
+        paymentMapper.insert(payment);
+
+        return PaymentConverter.toDTO(payment);
+    }
+
+    @Override
+    @Transactional
+    public PaymentDTOResponse updatePayment(Integer id, PaymentDTORequest request) {
+        Payment payment = paymentMapper.findById(id);
+
+        if (payment == null ){
+            throw new RuntimeException("Không tìm thấy Payment ID: " + id);
+        }
+
+        if (request.getPaymentMethod() != null) payment.setPaymentMethod(request.getPaymentMethod());
+        if (request.getAmount() != null) payment.setAmount(request.getAmount());
+        if (request.getTransactionCode() != null) payment.setTransactionCode(request.getTransactionCode());
+
+        paymentMapper.update(payment);
+
+        return PaymentConverter.toDTO(payment);
+    }
+
+    @Override
+    @Transactional
+    public int deletePayment(Integer id) {
+        Payment payment = paymentMapper.findById(id);
+        if (payment == null) {
+            throw new RuntimeException("Không tìm thấy Payment ID: "+ id);
+        }
+        return paymentMapper.deleteById(id);
+    }
+
+    @Override
+    public PaymentDTOResponse findById(Integer id) {
+        Payment payment = paymentMapper.findById(id);
+        if (payment == null) {
+            throw new RuntimeException("Không tìm thấy Payment ID: "+ id);
+        }
+        return PaymentConverter.toDTO(payment);
+    }
+
+    @Override
+    public PaymentDTOResponse findByCode(String transactionCode) {
+        Payment payment = paymentMapper.findByCode(transactionCode);
+        if (payment == null) {
+            throw new RuntimeException("Mã giao dịch không tồn tại!");
+        }
+        return PaymentConverter.toDTO(payment);
+    }
+}
