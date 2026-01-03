6a9fedd1d72a5225f81c6c127e1726bdf3f251bb
atus <tudaps39496@gmail.com>
2026-01-03 21:12:03 +0700

    UserAuthMapper.java

--- src/main/java/com/fpl/edu/shoeStore/auth/mapper/UserAuthMapper.java
+++ src/main/java/com/fpl/edu/shoeStore/auth/mapper/UserAuthMapper.java
@@ -1,24 +1,24 @@
 package com.fpl.edu.shoeStore.auth.mapper;
 
-
-import com.fpl.edu.shoeStore.user.entity.User;
-
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import com.fpl.edu.shoeStore.auth.entity.UserAuth;
+import com.fpl.edu.shoeStore.user.entity.User;
 
 @Mapper
 public interface UserAuthMapper {
-    UserAuth findUserByUsername(@Param("username") String username);
     
+    // Tìm người dùng để phục vụ Login (Spring Security)
+    UserAuth findUserByUsername(@Param("username") String username);
+    // Lấy Role ID của người dùng
     Integer getRoleIdByUsername(@Param("username") String username);
     
-    Integer existsByUsername(@Param("username") String username);
+    boolean existsByUsername(@Param("username") String username);
     
-    Integer existsByEmail(@Param("email") String email);
+    boolean existsByEmail(@Param("email") String email);
     
-    Integer existsByPhone(@Param("phone") String phone);
+    boolean existsByPhone(@Param("phone") String phone);
     
     int insertUser(User user);
 }
